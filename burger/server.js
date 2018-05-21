5065af38b6036388a4e3b649499d8381df59073a
canbat <lin.jack55@gmail.com>
2018-05-21 00:01:14 -0400

    second commit

---
+++ burger/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var PORT = process.env.NODE_ENV || 8080;
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT,function(){
+  console.log("Application is running on PORT %s", PORT);
+});
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured) values 
+("50/50 Burger",0),
+("Angus burger",0),
+("Banquet burger",0),
+("Butter burger",0);
